0a522acaa9faecbdf3d9ca42c9646ed30e24f5dc
Hyunjun Oh_nt <tronia94@gmail.com>
2018-09-17 17:04:59 +0900

    Drill 05 뼈대 완성
    points에 이동할 점들의 위치를 저장하고
    해당 점으로 이동시킬함수의
    뼈대를 만들었다.

--- Drills/Drill 05/Drill 05.py
+++ Drills/Drill 05/Drill 05.py
@@ -4,7 +4,15 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
+points = [203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92], [692, 518], [682, 336], [712, 349]
 
+def move_to_point(x, y):
+    pass
 
+x = 30
+y = 30
+
+while 1:
+    move_to_point(x, y)
 close_canvas()
 
