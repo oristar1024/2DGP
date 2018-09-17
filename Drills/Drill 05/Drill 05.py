cb20fcbc41a4d3396f011c6b20c7af272fb364eb
Hyunjun Oh_nt <tronia94@gmail.com>
2018-09-17 17:14:27 +0900

    Drill 05 점이동 완성
    하나의 점으로 캐릭터를 이동시켜봤다.

--- Drills/Drill 05/Drill 05.py
+++ Drills/Drill 05/Drill 05.py
@@ -6,13 +6,25 @@ character = load_image('animation_sheet.png')
 
 points = [203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92], [692, 518], [682, 336], [712, 349]
 
-def move_to_point(x, y):
-    pass
-
+frame = 0
+motion = 100 # 캐릭터의 이동방향을 표현하는 스프라이트 시트의 y값
 x = 30
 y = 30
 
+def move_to_point(px, py):
+    global x, y, frame, motion
+    xmove = (px - x) / 40
+    ymove = (py - y) / 40
+    for i in range (1, 40+1):
+        clear_canvas()
+        character.clip_draw(frame * 100, motion, 100, 100, x, y)
+        update_canvas()
+        x += xmove
+        y += ymove
+        frame = (frame + 1) % 8
+        delay(0.05)
+
 while 1:
-    move_to_point(x, y)
+    move_to_point(300, 300)
 close_canvas()
 
