e09602fa14ed0c24cd334b6af141749b57d74dbf
Hyunjun Oh_nt <tronia94@gmail.com>
2018-09-17 19:43:19 +0900

    Drill 05 완성 (commit description 수정)
    2014182026 오현준
    커밋횟수 : 5회
    https://www.youtube.com/watch?v=BqcnpKoXyxI&feature=youtu.be

--- Drills/Drill 05/Drill 05.py
+++ Drills/Drill 05/Drill 05.py
@@ -8,14 +8,14 @@ points = [203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225],
 
 frame = 0
 motion = 100 # 캐릭터의 이동방향을 표현하는 스프라이트 시트의 y값
-x = 30
-y = 30
+x = 25
+y = 50
 
 def move_to_point(px, py):
     global x, y, frame, motion
     xmove = (px - x) / 40 # 현재점에서 다음점까지의 증가값을 40개로 나눴다.
     ymove = (py - y) / 40
-    for i in range (1, 40+1):
+    for i in range (1, 40+1): # 40번에걸쳐 다음점으로 이동
         if xmove < 0: # 왼쪽으로 이동해야할때
             motion = 0
         else:
@@ -32,5 +32,6 @@ def move_to_point(px, py):
 while 1:
     for px, py in points:
         move_to_point(px, py)
+        
 close_canvas()
 
