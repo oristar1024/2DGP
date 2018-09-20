1cb282879784999727fdbdba94777d7883fccccb
Hyunjun Oh_nt <tronia94@gmail.com>
2018-09-20 16:55:24 +0900

    점이동 함수 구현
    Drill 05에서 사용했던 점으로 이동하는
    함수를 변형해서 적용했다.

--- Drills/Drill 06/Drill 06.py
+++ Drills/Drill 06/Drill 06.py
@@ -2,10 +2,30 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
+def move_to_point(px, py):
+    global cx, cy, frame, motion
+    global move = True;
+    xmove = (px - cx) / 40  # 현재점에서 다음점까지의 증가값을 40개로 나눴다.
+    ymove = (py - cy) / 40
+    for i in range(1, 40 + 1):  # 40번에걸쳐 다음점으로 이동
+        if move:
+            if xmove < 0:  # 왼쪽으로 이동해야할때
+                motion = 0
+            else:
+                motion = 100
+            clear_canvas()
+            character.clip_draw(frame * 100, motion, 100, 100, cx, cy)
+            update_canvas()
+            cx += xmove
+            cy += ymove
+            frame = (frame + 1) % 8
+            delay(0.05)
+
 
 def handle_events():
     global running
     global mx, my
+    global cx, cy
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -14,6 +34,9 @@ def handle_events():
             mx, my = event.x + 50, KPU_HEIGHT - 1 - event.y - 50
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            pass
+
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -26,13 +49,13 @@ mx, my = KPU_WIDTH // 2, KPU_HEIGHT // 2
 cx, cy = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 motion = 1
+move = False
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     mouse.clip_draw(0, 0, 100, 100, mx, my)
-    character.clip_draw(frame * 100, 100 * motion, 100, 100, cx, cy)
     update_canvas()
     frame = (frame + 1) % 8
 
