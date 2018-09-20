f900f04157edc15d8e312821b495d439a12b87d5
Hyunjun Oh_nt <tronia94@gmail.com>
2018-09-20 16:47:18 +0900

    Drill 06 뼈대완성
    실습을 수정해서 Drill 06의 뼈대를
    만들었다

--- Drills/Drill 06/Drill 06.py
+++ Drills/Drill 06/Drill 06.py
@@ -5,30 +5,34 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
-    global x, y
+    global mx, my
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            mx, my = event.x + 50, KPU_HEIGHT - 1 - event.y - 50
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
+mouse = load_image('hand_arrow.png')
 character = load_image('animation_sheet.png')
 
 running = True
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+mx, my = KPU_WIDTH // 2, KPU_HEIGHT // 2
+cx, cy = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
+motion = 1
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    mouse.clip_draw(0, 0, 100, 100, mx, my)
+    character.clip_draw(frame * 100, 100 * motion, 100, 100, cx, cy)
     update_canvas()
     frame = (frame + 1) % 8
 
