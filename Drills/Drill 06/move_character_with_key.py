d6ba7ff1d53191fa063ca2e8a3b3ea71d1c85772
Hyunjun Oh_nt <tronia94@gmail.com>
2018-09-20 16:20:04 +0900

    키입력 실습 수정
    키보드 입력으로 캐릭터를 이동시키는
    실습에 왼쪽으로 달리는 애니메이션을
    추가하였다.

--- Drills/Drill 06/move_character_with_key.py
+++ Drills/Drill 06/move_character_with_key.py
@@ -4,6 +4,7 @@ from pico2d import *
 def handle_events():
     global running
     global dir
+    global motion
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -11,8 +12,10 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir += 1
+                motion = 1
             elif event.key == SDLK_LEFT:
                 dir -= 1
+                motion = 0
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -30,11 +33,12 @@ running = True
 x = 800 // 2
 frame = 0
 dir = 0
+motion = 1
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 100 * motion, 100, 100, x, 90)
     update_canvas()
 
     handle_events()
