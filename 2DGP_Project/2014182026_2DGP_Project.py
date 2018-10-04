44f08702226a344e94c6c4c21138c9b38e618965
Hyunjun Oh_nt <tronia94@gmail.com>
2018-10-04 15:39:56 +0900

    캐릭터 스프라이트 좌표 완료
    이동시 필요한 캐릭터 스프라이트의
    좌표값를 찾아냈다.

--- 2DGP_Project/2014182026_2DGP_Project.py
+++ 2DGP_Project/2014182026_2DGP_Project.py
@@ -2,8 +2,8 @@ from pico2d import *
 import random
 weapon = 1
 running = True
-# 캐릭터 스프라이트의 머리는 0, 900 부터 X 40, Y 30 (글자위치 10)
-# 몸통은 850부터 동일, 머리로부터 Y를 15만큼 빼준다.
+# 캐릭터 스프라이트의 머리는 5, 900 부터 X 40, Y 30 (글자위치 10)
+# 몸통은 8, 850부터 X32, Y30, 머리로부터 Y를 15만큼 빼준다.
 
 def handle_events():
     global running
@@ -23,7 +23,7 @@ elif weapon == 2:
 elif weapon == 3:
     character = load_image('cannoneer.png')
 while running:
-    character.clip_draw(15, 850, 20, 30, 100, 100 - 15)
-    character.clip_draw(0, 900, 40, 30, 100, 100)
+    character.clip_draw(8 + 32*7, 850, 32, 30, 100, 100 - 15)
+    character.clip_draw(5, 900, 40, 30, 100, 100)
     update_canvas()
     handle_events()
