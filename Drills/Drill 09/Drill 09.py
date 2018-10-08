72a6765c23d7cf25421cc3f750fbbe13685653c8
Hyunjun Oh_nt <tronia94@gmail.com>
2018-10-08 17:02:04 +0900

    Drill 09 거의 완성
    볼이 바닥에서 튕기는듯 보이는게 마음에안든다.

--- Drills/Drill 09/Drill 09.py
+++ Drills/Drill 09/Drill 09.py
@@ -8,6 +8,7 @@ class Grass:
     def draw(self):
         self.image.draw(400, 30)
 
+
 class Boy:
     def __init__(self):
         self.x, self.y = random.randint(100, 700), 90
@@ -21,24 +22,35 @@ class Boy:
     def draw(self):
         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
+
 class Ball:
     def __init__(self):
-        self.x, self.y = random.randint(50, 750), 550
+        self.x, self. y = random.randint(50, 750), 550
         self.speed = random.uniform(5, 15)
         self.big = random.randint(0, 1)
         if self.big == 1:
-            self.image = load_image('ball41x41.png')
+             self.image = load_image('ball41x41.png')
         else:
-            self.image = load_image('ball21x21.png')
+             self.image = load_image('ball21x21.png')
 
     def update(self):
-        pass
+        if self.big == 1:
+            if self.y > 70:
+                self.y -= self.speed
+            else:
+                self.y = 70
+        else:
+            if self.y > 60:
+                self.y -= self.speed
+            else:
+                self.y = 60
 
     def draw(self):
         if self.big == 1:
             self.image.clip_draw(0, 0, 41, 41, self.x, self.y)
         else:
             self.image.clip_draw(0, 0, 21, 21, self.x, self.y)
+
 
 def handle_events():
     global running
@@ -49,29 +61,29 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
 open_canvas()
 
-grass = Grass()
 team = [Boy() for i in range(11)]
 balls = [Ball() for i in range(20)]
+grass = Grass()
 
 running = True
 
 while running:
     handle_events()
+
     for boy in team:
         boy.update()
     for ball in balls:
         ball.update()
 
     clear_canvas()
-    
     grass.draw()
     for boy in team:
         boy.draw()
     for ball in balls:
         ball.draw()
-
     update_canvas()
 
     delay(0.05)
