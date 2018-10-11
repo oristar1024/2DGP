581a7683d06ae1ae93e7df9ca84aa0cfabc4d502
Hyunjun Oh_nt <tronia94@gmail.com>
2018-10-11 16:36:47 +0900

    실습 구현완료
    강의 진행중에 실습을 구현완료했다.

--- Drills/Drill 10/start_state.py
+++ Drills/Drill 10/start_state.py
@@ -8,19 +8,32 @@ logo_time = 0.0
 
 
 def enter():
-    pass
+    global image
+    image = load_image('kpu_credit.png')
 
 
 def exit():
-    pass
+    global image
+    del(image)
 
+import title_state
 
 def update():
-    pass
+    global logo_time
+
+    if logo_time > 1.0 :
+        logo_time = 0
+        #game_framework.quit()
+        game_framework.change_state(title_state)
+    delay(0.01)
+    logo_time += 0.01
 
 
 def draw():
-    pass
+    global image
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
 
 
 
--- Drills/Drill 10/title_state.py
+++ Drills/Drill 10/title_state.py
@@ -7,19 +7,32 @@ image = None
 
 
 def enter():
-    pass
+    global image
+    image = load_image('title.png')
 
 
 def exit():
-    pass
+    global image
+    del(image)
 
+import main_state
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+                game_framework.change_state(main_state)
 
 
 def draw():
-    pass
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
 
 
 
