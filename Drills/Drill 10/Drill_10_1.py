eb1aaac745415ef71f674e4f526b1e9801b8b616
Hyunjun Oh_nt <tronia94@gmail.com>
2018-10-11 20:18:42 +0900

    handle events 함수 완성
    p를누르면 이전씬으로 돌아가게했다.

--- Drills/Drill 10/Drill_10_1.py
+++ Drills/Drill 10/Drill_10_1.py
@@ -16,7 +16,10 @@ def exit():
 
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
 
 
 def draw():
