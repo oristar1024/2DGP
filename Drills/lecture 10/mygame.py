e5eca7eefe98f8ef78401e182cfb1946a32886d3
Hyunjun Oh_nt <tronia94@gmail.com>
2018-10-23 14:47:20 +0900

    game framework 적용
    수업시간에 배운 프레임워크를 적용했다.

--- Drills/lecture 10/mygame.py
+++ Drills/lecture 10/mygame.py
@@ -3,9 +3,6 @@ import pico2d
 
 import main_state
 
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 768
-
-pico2d.open_canvas(SCREEN_WIDTH, SCREEN_HEIGHT)
+pico2d.open_canvas()
 game_framework.run(main_state)
 pico2d.close_canvas()
