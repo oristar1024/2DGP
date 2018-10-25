2990f6813979b29d4536373bc86e53e5478a7d4e
Hyunjun Oh_nt <tronia94@gmail.com>
2018-10-25 16:08:08 +0900

    실습 구현완료
    lecture 11의 실습구현을 마쳤다.

--- Drills/Drill 11/boys_team_1000.py
+++ Drills/Drill 11/boys_team_1000.py
@@ -11,12 +11,11 @@ class Grass:
 
 
 class Boy:
-    image = None
+
     def __init__(self):
         self.x, self.y = random.randint(100, 700), 90
         self.frame = random.randint(0, 7)
-        if Boy.image == None:
-            Boy.image = load_image('run_animation.png')
+        self.image = load_image('run_animation.png')
 
 
     def update(self):
