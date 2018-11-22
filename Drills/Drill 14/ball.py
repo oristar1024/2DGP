c6bac1e7fba6db2f7d3716f48d587907adc78a8d
Hyunjun Oh_nt <tronia94@gmail.com>
2018-11-22 16:27:11 +0900

    볼의 y위치 랜덤설정
    y좌표도 랜덤으로 나온다.

--- Drills/Drill 14/ball.py
+++ Drills/Drill 14/ball.py
@@ -9,7 +9,7 @@ class Ball:
     def __init__(self):
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
-        self.x, self.y, self.fall_speed = random.randint(0, 1600-1), 60, 0
+        self.x, self.y, self.fall_speed = random.randint(250, 1600), random.randint(50, 1050), 0
 
     def get_bb(self):
         return self.x - 10, self.y - 10, self.x + 10, self.y + 10
