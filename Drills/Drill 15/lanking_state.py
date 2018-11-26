1bd0d05571a857c9d1b1a2de964bd69db19fd1a3
Hyunjun Oh_nt <tronia94@gmail.com>
2018-11-26 18:02:13 +0900

    Drill 15 완성
    2014182026 오현준
    커밋횟수 : 3회
    https://youtu.be/PZ54Etq7gPg

--- Drills/Drill 15/lanking_state.py
+++ Drills/Drill 15/lanking_state.py
@@ -7,21 +7,55 @@ from pico2d import *
 import game_framework
 import game_world
 import main_state
+import world_build_state
 
 name = "LankingState"
 
-def enter():
-    with open('langking_data.json' , 'r') as f:
-        lanking_list = json.load(f)
+ranking_list = None
+font = None
 
+def enter():
+    global font
+    global ranking_list
+    font = load_font('ENCR10B.TTF', 20)
+    with open('lanking_data.json' , 'rb') as f:
+        ranking_list = json.load(f)
     game_rank = None
-    for data in lanking_list:
+    for data in ranking_list:
         if data['record'] < main_state.game_record:
             game_rank = data['rank']
-            while game_rank < 10:
-                lanking_list[game_rank]['record'] = lanking_list[game_rank - 1]['record']
+            i = 9
+            while i >= game_rank:
+                ranking_list[game_rank]['record'] = ranking_list[game_rank - 1]['record']
+                i -= 1
             data['record'] = main_state.game_record
             break
+    rank_list = json.dumps(ranking_list)
+    f = open("lanking_data.json", 'w')
+    f.write(rank_list)
+    f.close()
 
-    with open('lanking_data.json', 'wb') as f:
-        pickle.dump(lanking_list, f)
+def update():
+    pass
+
+def draw():
+    global ranking_list
+    global font
+    clear_canvas()
+    i = 600
+    for data in ranking_list:
+        font.draw(300, i, 'rank: %d' % data['rank'], (0, 0, 0))
+        font.draw(600, i, 'record: %3f' % data['record'], (0, 0, 0))
+        i-= 30
+    update_canvas()
+
+def exit():
+    pass
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(world_build_state)
